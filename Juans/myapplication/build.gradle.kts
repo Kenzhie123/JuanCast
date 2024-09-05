12607b65434b40a5f1cebd83b01f290df7f2ceb4
Basan266 <167392157+Basan266@users.noreply.github.com>
2024-09-05 10:42:38 +0800

    Mga nadagdag

--- Juans/myapplication/build.gradle.kts
+++ Juans/myapplication/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.JuanCast.myapplication"
         minSdk = 24
         targetSdk = 34
-        versionCode = 30
+        versionCode = 40
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
